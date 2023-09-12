f32bc51af2f4ef253242c657f7ec5b0b3be916c6
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2023-09-13 00:03:24 +0200

    add example

--- src/main/java/org/examples/HelloTensorFlow/HelloTensorFlow.java
+++ src/main/java/org/examples/HelloTensorFlow/HelloTensorFlow.java
@@ -1,15 +1,15 @@
 package org.examples.HelloTensorFlow;
 
-import org.tensorflow.ConcreteFunction;
-import org.tensorflow.Signature;
-import org.tensorflow.Tensor;
-import org.tensorflow.TensorFlow;
+import org.tensorflow.*;
 import org.tensorflow.op.Ops;
 import org.tensorflow.op.core.Placeholder;
 import org.tensorflow.op.math.Add;
+import org.tensorflow.op.math.Square;
 import org.tensorflow.types.TInt32;
 
+
 public class HelloTensorFlow {
+
 
     public static void main(String[] args) {
         System.out.println("Hello TensorFlow " + TensorFlow.version());
@@ -22,6 +22,15 @@ public class HelloTensorFlow {
              Tensor dblX = doubleInteger.call(x)) {
             System.out.println(x.getInt() + " doubled is " + ((TInt32) dblX).getInt());
         }
+
+        //tries to create a ConcreteFunction that squares an integer.
+        try (ConcreteFunction squareInteger = ConcreteFunction.create(HelloTensorFlow::squared);
+             //creates a TensorFlow scalar tensor with the value 10
+             TInt32 x = TInt32.scalarOf(1000);
+             //calls the ConcreteFunction with the input tensor x, which contains the value 10 -> performs the operation
+             Tensor squareX = squareInteger.call(x)) {
+            System.out.println(x.getInt() + " doubled is " + ((TInt32) squareX).getInt());
+        }
     }
 
     private static Signature doubleInteger(Ops tf) {
@@ -31,6 +40,15 @@ public class HelloTensorFlow {
         Add<TInt32> dblX = tf.math.add(x, x);
         //defines the inputs and outputs which are used in our main method, using a Signature
         return Signature.builder().input("x", x).output("doubleInteger", dblX).build();
+    }
+
+    private static Signature squared(Ops tf) {
+        //creates placeholder for input value
+        Placeholder<TInt32> x = tf.placeholder(TInt32.class);
+        //uses the TensorFlow square operation
+        Square<TInt32> squareX = tf.math.square(x);
+        //defines the inputs and outputs which are used in our main method, using a Signature
+        return Signature.builder().input("x", x).output("squared", squareX).build();
     }
 
 }
